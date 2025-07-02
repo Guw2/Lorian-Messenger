83b7386916849ee71ee495c189bb2e37ba85cc0b
Guw2 <gustafar79@gmail.com>
2025-07-02 14:50:49 -0300

    9th Commit - Enabling RabbitMQ in Both Services

--- auth-service/src/main/java/com/lorian/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/lorian/auth_service/AuthServiceApplication.java
@@ -1,9 +1,11 @@
 package com.lorian.auth_service;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
